7fdf3168337944d25a8841d9d6968184ed279d80
sophiacheong <sophiaacheong@gmail.com>
2021-05-18 19:51:50 -0700

    created axios.get function for repo

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,22 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (user) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/${user}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
-
+  axios.get(options)
+    .then((res) => console.log(res.data))
+    .catch((err) => console.error(err));
 }
 
 module.exports.getReposByUsername = getReposByUsername;
