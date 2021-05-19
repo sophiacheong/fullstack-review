3b6ebbcbc4cac01a13c257697a6ea1e9c77e2a86
sophiacheong <sophiaacheong@gmail.com>
2021-05-18 19:26:44 -0700

    added repoSchema and function to save repos into the db

--- database/index.js
+++ database/index.js
@@ -2,15 +2,35 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  owner: {
+    login: String,
+    id: Number,
+  },
+  html_url: String,
+  url: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (data) => {
+  data.forEach((item) => {
+    let schemaData = {
+      id: item.id,
+      name: item.name,
+      owner: {
+        login: item.owner.login,
+        id: item.owner.id,
+      },
+      html_url: item.html_url,
+      url: item.url,
+    }
+  })
+
+  Repo.create(schemaData, (err, results) => {
+    if (err) { console.error(err); } else { console.log(results, ' has been added'); }
+  })
 }
 
 module.exports.save = save;
