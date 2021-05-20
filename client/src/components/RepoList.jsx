ede0c34894d2ea896bee31832f8af96aad076bc3
sophiacheong <sophiaacheong@gmail.com>
2021-05-19 20:02:22 -0700

    displayed the top 25 links on front page

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,6 +4,11 @@ const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+    {props.repos.map((item, index) => (
+      <div>
+        <a href={item.html_url} target="_blank">{item.name}</a>
+      </div>
+    ))}
   </div>
 )
 
